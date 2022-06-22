64ab216b91805d2356415c28a1b5a4f81eff150b
lazy-sky <1cielo@naver.com>
2022-06-23 00:29:10 +0900

    Design: Edit Layout styles

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,15 @@
 import Footer from '../footer'
 import Header from '../header'
 
+import styles from './layout.module.scss'
+
 interface ILayoutProps {
   children: React.ReactNode
 }
 
 const Layout = ({ children }: ILayoutProps) => {
   return (
-    <div>
+    <div className={styles.layout}>
       <Header />
       <main>{children}</main>
       <Footer />
