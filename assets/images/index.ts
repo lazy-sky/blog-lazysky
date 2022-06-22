c9d8865df71b4a305fe0550f84237c1eb6f3ce58
lazy-sky <1cielo@naver.com>
2022-06-23 00:03:19 +0900

    Asset: Upload an image

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -3,5 +3,13 @@ import InstagramIcon from './icon-instagram.svg'
 import MenuIcon from './icon-menu.svg'
 import FaceLogo from './logo-face.png'
 import TorsoLogo from './logo-torso.png'
+import ArrowLeftIcon from './arrow-left.svg'
 
-export { GithubIcon, InstagramIcon, MenuIcon, FaceLogo, TorsoLogo }
+export {
+  GithubIcon,
+  InstagramIcon,
+  MenuIcon,
+  FaceLogo,
+  TorsoLogo,
+  ArrowLeftIcon,
+}
