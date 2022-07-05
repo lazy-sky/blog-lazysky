f0c56565fcbabd4cf1840afd8fa4cf4f8dab4009
lazy-sky <1cielo@naver.com>
2022-07-05 17:49:42 +0900

    Feat: Make IPost

---
+++ types/post.d.ts
@@ -0,0 +1,13 @@
+interface IPost {
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  excerpt: string
+  ogImage: {
+    url: string
+  }
+  content: string
+}
+
+export default IPost
