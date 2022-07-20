0949508a85d2cef33edeb33c5e61c4a9e957b686
lazy-sky <1cielo@naver.com>
2022-07-20 21:45:24 +0900

    Feat: Render logo in Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { TorsoLogo } from 'assets/images'
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +12,17 @@ const Home: NextPage = () => {
         <meta name='description' content='Blog made by next and notion' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <div style={{ padding: '50px' }}>
+        <Image
+          priority
+          src={TorsoLogo}
+          width='100%'
+          height='100%'
+          layout='responsive'
+          objectFit='contain'
+          alt='logo'
+        />
+      </div>
     </div>
   )
 }
