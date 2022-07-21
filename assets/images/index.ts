9af31df203c2508aaaba80fd3cbae01776284d2b
lazy-sky <1cielo@naver.com>
2022-07-21 14:59:12 +0900

    Asset: Upload images

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,15 +1,21 @@
 import GithubIcon from './icon-github.svg'
 import InstagramIcon from './icon-instagram.svg'
 import MenuIcon from './icon-menu.svg'
+import PostIcon from './icon-post.png'
+import ProjectIcon from './icon-project.png'
 import FaceLogo from './logo-face.png'
 import TorsoLogo from './logo-torso.png'
+import TorsoLogoSVG from './logo-torso.svg'
 import ArrowLeftIcon from './arrow-left.svg'
 
 export {
   GithubIcon,
   InstagramIcon,
   MenuIcon,
+  PostIcon,
+  ProjectIcon,
   FaceLogo,
   TorsoLogo,
+  TorsoLogoSVG,
   ArrowLeftIcon,
 }
