3b0bf8643ca2b88d1c5e4b8c532a4f3956aa028b
lazy-sky <1cielo@naver.com>
2022-07-22 16:56:54 +0900

    Conf: Try to fix build the error
    - Cannot read properties of null (reading 'childNodes')
    - Application error: a client-side exception has occurred (see the
      browser console for more information).

---
+++ .next/server/pages/404.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/404";
+exports.ids = ["pages/404"];
+exports.modules = {
+
+/***/ "./pages/404.tsx":
+/*!***********************!*\
+  !*** ./pages/404.tsx ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst PageNotFound = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"\\uD398\\uC774\\uC9C0\\uB97C \\uCC3E\\uC744\\uC218 \\uC5C6\\uC2B5\\uB2C8\\uB2E4.\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\lucid\\\\projects\\\\blog-with-notion\\\\pages\\\\404.tsx\",\n        lineNumber: 4,\n        columnNumber: 10\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (PageNotFound);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy80MDQudHN4LmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBRUEsTUFBTUEsWUFBWSxHQUFhLElBQU07SUFDbkMscUJBQU8sOERBQUNDLElBQUU7a0JBQUMsdUVBQWM7Ozs7O2lCQUFLO0NBQy9CO0FBRUQsaUVBQWVELFlBQVkiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9ibG9nLWxhenlza3kvLi9wYWdlcy80MDQudHN4P2NhNjciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHR5cGUgeyBOZXh0UGFnZSB9IGZyb20gJ25leHQnXHJcblxyXG5jb25zdCBQYWdlTm90Rm91bmQ6IE5leHRQYWdlID0gKCkgPT4ge1xyXG4gIHJldHVybiA8aDE+7Y6Y7J207KeA66W8IOywvuydhOyImCDsl4bsirXri4jri6QuPC9oMT5cclxufVxyXG5cclxuZXhwb3J0IGRlZmF1bHQgUGFnZU5vdEZvdW5kXHJcbiJdLCJuYW1lcyI6WyJQYWdlTm90Rm91bmQiLCJoMSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/404.tsx\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/404.tsx"));
+module.exports = __webpack_exports__;
+
+})();
