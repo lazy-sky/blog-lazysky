23f41a39beb2f45946fa5ae3d7b5dbdc2c85f8de
lazy-sky <1cielo@naver.com>
2022-07-24 23:47:31 +0900

    Feat: Set Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,26 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+import { pageview } from 'lib/ga'
 import Layout from 'components/layout'
 
 import 'styles/globals.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
       <Component {...pageProps} />
